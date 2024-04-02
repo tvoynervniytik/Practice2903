fc51f65c7b11df25af4126c142acd6d22823401c
Anastasiya <nastena.0536@gmail.com>
2024-04-02 18:18:03 +0300

    Сделана вёрстка главной страницы и переходы

--- Practice2903/MainWindow.xaml.cs
+++ Practice2903/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +19,26 @@ namespace Practice2903
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// #d5c847 #fdb249 #fed7c1 #eed4fe
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            frNav.Navigate(new Pages.MainPage());
+        }
+
+        private void Hyperlink_Click_1(object sender, RoutedEventArgs e)
+        {
+            frNav.NavigationService.Navigate(new Pages.ListOfDishes());
+        }
+        private void Hyperlink_Click_2(object sender, RoutedEventArgs e)
+        {
+            frNav.NavigationService.Navigate(new Pages.ListOfIngredients());
+        }
+        private void Hyperlink_Click_3(object sender, RoutedEventArgs e)
+        {
+            frNav.NavigationService.Navigate(new Pages.ListOfOrders());
         }
     }
 }
