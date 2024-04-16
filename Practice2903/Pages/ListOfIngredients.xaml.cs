fee719bf72d9b81ee43125d1d30d984a3bb604c6
Пресс-секретарь <-@DESKTOP-BM2FERE>
2024-04-16 08:56:13 +0300

    /

--- Practice2903/Pages/ListOfIngredients.xaml.cs
+++ Practice2903/Pages/ListOfIngredients.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,8 +26,75 @@ namespace Practice2903.Pages
         public ListOfIngredients()
         {
             InitializeComponent();
-            ingredients = new List<Ingredient>(DBConnection.practice.Ingredient.ToList());
-            this.DataContext = this;
+            Refresh();
         }
+        private void HLDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var ingredient = (sender as Hyperlink).DataContext as Ingredient;
+            try
+            {
+                DBConnection.practice.Ingredient.Remove(ingredient);
+                DBConnection.practice.SaveChanges();
+            }
+            catch
+            {
+                MessageBox.Show("This product cannot be removed");
+            }
+
+            Refresh();
+        }
+        private void TBСount_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[0-9]");
+            if (!regex.IsMatch(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+        private void Refresh()
+        {
+            var ingredients = DBConnection.practice.Ingredient.ToList();
+            double result = 0;
+            foreach (var i in ingredients)
+            {
+                double c = i.PriceInDollars * i.AvailableCount;
+                result += c;
+            }
+            costTb.Text = result.ToString();
+            LVIngredients.ItemsSource = ingredients;
+
+        }
+        private void BPlus_Click(object sender, RoutedEventArgs e)
+        {
+            var ingredient = (sender as Button).DataContext as Ingredient;
+            if (ingredient.AvailableCount == 99)
+                return;
+            ingredient.AvailableCount += 1;
+            DBConnection.practice.SaveChanges();
+            Refresh();
+        }
+        private void BMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var ingredient = (sender as Button).DataContext as Ingredient;
+            try
+            {
+                if (ingredient.AvailableCount == 0)
+                    return;
+                ingredient.AvailableCount -= 1;
+                DBConnection.practice.SaveChanges();
+
+            }
+            catch
+            {
+                MessageBox.Show("Error!");
+            }
+
+            Refresh();
+        }
+        private void TBСount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            DBConnection.practice.SaveChanges();
+        }
+
     }
 }
